cb31027072551c75a7bde200adb886f1448a9809
Norton Ricardo <norton.ricardo@passwordinfo.com.br>
2022-09-06 17:39:15 -0300

    create homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'home#home'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :home do
+    # get 'dashboard', on: :collection
+  end
+
 end
